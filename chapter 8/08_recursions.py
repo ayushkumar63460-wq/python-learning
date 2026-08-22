477003b6abf7da236f37aa2e17dacb4343cc13fa
ㅤAyush <ayushkumar63460@gmail.com>
2026-08-22 19:45:45 +0530

    Rest of the Recursive problems and types done...

--- chapter 8/08_recursions.py
+++ chapter 8/08_recursions.py
@@ -12,9 +12,92 @@ which says STOP we are done otherwise error beacuse goes on forever.'''
 
 
 def factorial(n):
-    if n==0 or n==1:
+    if n==0:
         return 1
     else:
         return n*factorial(n-1)
 
 print(factorial(5))
+
+
+'''def function(n):
+
+    if stopping_condition:
+        return
+
+    # do some work
+
+    function(smaller_problem)'''
+
+
+
+
+'''SUM OF NUMBERS'''
+def sum_num(n):
+    if n == 0:
+        return 0
+    return n + sum_num(n-1)
+
+print(sum_num(5))
+
+
+'''COUNTING WITH RECURSION'''
+def count_digit(n):
+    if n == 0:
+        return 0
+    return 1 + count_digit(n // 10)    
+print(count_digit(8999))
+
+
+'''POWER RECURSION'''
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    return base * power(base, exponent - 1)
+print(power(2, 3))
+    
+
+'''RECURSION WITH STRINGS'''
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return reverse(text[1:]) + text[0]
+
+print(reverse("Making"))
+
+
+'''RECURSION WITH LISTS'''
+def list_num(numbers):
+    if len(numbers) == 0:
+        return 0
+    return numbers[0] + list_num(numbers[1:])
+print(list_num([10, 20, 30, 40]))
+
+
+'''FIBONACCI'''
+def fibonaci(n):
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+    return fibonaci(n-1) + fibonaci(n-2)
+print(fibonaci(5))
+
+
+'''1. Write the first call.
+2. Keep expanding recursive calls.
+3. Stop at the base case.
+4. Start returning upward.
+5. Substitute returned values.'''
+
+
+'''count vowels recursively'''
+def count_vow(text):
+    if text == "":
+        return 0
+    if text[0] in "aeiou":
+        return 1 + count_vow(text[1:])
+    return count_vow(text[1:])
+
+print(count_vow("Making"))
